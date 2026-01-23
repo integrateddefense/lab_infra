1313c0ed8c69d78b3f80ca09cecd0ec4e28dd5b0
James Thomas <james.b.thomas@mac.com>
2026-01-23 10:14:47 -0500

    Publish: 2026-01-23T10:14:47-05:00

--- ansible/roles/infrastructure/distro_switch/tasks/cisco.py
+++
@@ -1,7 +0,0 @@
-# Role to configure cisco access switches
-## Primarily covers C1200 switches on the Cisco SMB line
-
-# TODO
-## Primary Module: community.ciscosmb.com
-## Approach: Break config into sections, then apply authoritative config
-##   On Cisco, commands are mostly atomic so don't need to worry about externally applying idempotency
